2cac2a6a611168e021dd0c6d2ebe91d08afb9a62
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-12-06 19:01:25 +0100

    initial commit, klasa Grad

---
+++ src/ba/unsa/etf/tutorijal7/Grad.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.tutorijal7;
+
+import java.io.Serializable;
+
+public class Grad implements Serializable {
+    String naziv;
+    int brojStanovnika;
+    double[] temperature;
+
+    public Grad(){
+        setTemperature(new double[1000]);
+    }
+    public Grad(String naziv, int brojStanovnika, double[] temperature){
+        this.naziv=naziv;
+        this.brojStanovnika=brojStanovnika;
+        this.temperature=temperature;
+    }
+
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+}
