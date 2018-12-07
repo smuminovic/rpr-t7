788582fb27215a4a1127d988ebd9a554614e02d2
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-12-07 16:37:04 +0100

    metoda ucitajGradove

--- src/ba/unsa/etf/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/tutorijal7/Tutorijal.java
@@ -1,23 +1,54 @@
 package ba.unsa.etf.tutorijal7;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Tutorijal extends Application {
+public class Tutorijal {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public static ArrayList<Grad> ucitajGradove() {
+        ArrayList<Grad> gradovi = new ArrayList<>();
+        Scanner ulaz;
+        String zarez = ",";
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka se ne može otvoriti.");
+            return gradovi;
+        }
+        try {
+            while (ulaz.hasNextLine()) {
+                int br=0;
+                String imeGrada = "";
+                String linija = ulaz.nextLine();
+                int i;
+                for (i = 0; i < linija.length(); i++) {
+                    if (linija.charAt(i) != zarez.charAt(0)) {
+                        imeGrada+=linija.charAt(i);
+                    }
+                    else {
+                        int j;
+                        double[] temperature = new double[1000];
+                        String temp = "";
+                        for (j = i; j < linija.length(); j++) {
+                            if (linija.charAt(j) != zarez.charAt(0)) {
+                                temp += linija.charAt(j);
+                                temperature[br] = Double.parseDouble(temp);
+                                br++;
+                            }
+                            Grad g = new Grad(imeGrada, 0, temperature);
+                            gradovi.add(g);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+        }
+        return gradovi;
     }
 
-
     public static void main(String[] args) {
-        launch(args);
     }
 }
